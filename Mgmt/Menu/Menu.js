3823ca84cc86fc0934b661bf7781949009fd5e2a
MetroOJT <kouhei-eguchi@metrobooks.co.jp>
2023-10-11 10:25:48 +0900

    test

---
+++ Mgmt/Menu/Menu.js
@@ -0,0 +1,28 @@
+﻿// var Ajax_File = "Index.ashx"
+
+// ログアウト
+function btnLogOutClick() {
+    console.log("ログアウト");
+}
+
+// イベント登録
+function btnEventRegClick() {
+    console.log("イベント登録");
+}
+
+// イベント検索
+function btnEventSearchClick() {
+    console.log("イベント検索");
+}
+
+// 通信ログ
+function btnComLogClick() {
+    console.log("通信ログ");
+}
+
+$(function () {
+    document.getElementById("btnLogOut").addEventListener("click", btnLogOutClick, false);
+    document.getElementById("btnEventReg").addEventListener("click", btnEventRegClick, false);
+    document.getElementById("btnEventSearch").addEventListener("click", btnEventSearchClick, false);
+    document.getElementById("btnComLog").addEventListener("click", btnComLogClick, false);
+});
