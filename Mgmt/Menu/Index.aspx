695e40380e6fcb080e6f758e9f9e99dda60fe21c
MetroOJT <kouhei-eguchi@metrobooks.co.jp>
2023-10-26 10:14:30 +0900

    俺が守るよ

--- Mgmt/Menu/Index.aspx
+++ Mgmt/Menu/Index.aspx
@@ -5,22 +5,15 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <link rel="stylesheet" href="Index.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <%="<script src='../../Common/js/Common.js?ts=" & cCom.CmnTimeStamp & "'></script>" %>
     <title>メニュー</title>
 </head>
 <body>
     <form id="form1" runat="server" style="width: 80%; margin: 0 auto;">
-    <div>
-        <div id="HeaderArea" class="d-flex p-3" style="height:150px">
-                <div class="w-50  h1 text-center m-0" style="vertical-align:middle">クーポン発行サービス</div>
-                <div class="w-50 text-end">
-                    <input type="button" id="btnLogOut" value="ログアウト" class="btn btn-warning"/>
-                    <p id="Manager" class="h5 m-0"></p>
-                </div>
-        </div>
-            <hr class="m-0"/>
-        <div id="option"></div>
+    <div class="container">
+        <%=cCom.CmnDspHeader() %>
+        <div id="option" style="width: 40%; display: flex; flex-direction: column; margin: 0 auto;"></div>
     </div>
     </form>
     <%--＜aspsファイルのheadタグの中に追加＞--%>
--- Mgmt/Menu/Index.js
+++ Mgmt/Menu/Index.js
@@ -7,31 +7,9 @@ const u_name = sessionStorage.getItem("UserName");
 
 window.onload = function () {
     function_login_check();
-    let MenuID = $("#txtMenuID").val();
-    let MenuName = $("#txtMenuName").val();
-    let Admin = $("#txtAdmin").val();
-
-    $.ajax({
-        url: Ajax_File,
-        method: "POST",
-        data: {
-            "mode": "CreateTable",
-            "MenuID": MenuID,
-            "MenuName": MenuName,
-            "Admin": Admin
-        },
-        dataType: "json",
-        success: function (data) {
-            if (data != "") {
-                if (data.status == "OK") {
-                    document.getElementById("Manager").textContent = "担当者名: " + u_name;
-                    MakeMenu();
-                } else {
-                    alert(data.status); 
-                };
-            };
-        }
-    });
+    DspLoginUserName();
+    let Admin = u_admin;
+    MakeMenu();
 }
 
 function MakeMenu() {
@@ -46,10 +24,8 @@ function MakeMenu() {
         success: function (data) {
             if (data != "") {
                 if (data.status == "OK") {
-                    console.log("mm実行完了")
                     document.getElementById("option").innerHTML = data.html;
                 } else {
-                    console.log("mmエラー")
                     alert(data.status);
                 };
             };
@@ -57,64 +33,6 @@ function MakeMenu() {
     });
 }
 
-// ログアウト
-function btnLogOutClick() {
-    // ＜ログアウトをしたときの処理に追加＞
-    sessionStorage.removeItem("unauthorized_access");
-    console.log("ログアウト");
-    window.location = "../Login/Index.aspx";
+function transition(url) {
+    window.location = url;
 }
-
-// イベント登録
-function btnEventRegClick() {
-    console.log("イベント登録");
-    window.location = "../Event/Detail.aspx";
-}
-
-// イベント検索
-function btnEventSearchClick() {
-    console.log("イベント検索");
-    window.location = "../Event/Index.aspx";
-}
-
-// 通信ログ
-function btnComLogClick() {
-    console.log("通信ログ");
-    window.location = "../CommLog/Index.aspx";
-}
-
-$(function () {
-    document.getElementById("btnLogOut").addEventListener("click", btnLogOutClick, false);
-});
-
-//window.onload = function () {
-    //    let UserID = $("#txtUserID").val();
-    //    let UserName = $("#txtUserName").val();
-    //    let Admin = $("#txtAdmin").val();
-
-    //    $.ajax({
-    //        url: Ajax_File,
-    //        method: "POST",
-    //        data: {
-    //            "admin": "1",
-    //            "UserID": UserID,
-    //            "UserName": UserName,
-    //            "Admin": Admin
-    //        },
-    //        dataType: "json",
-    //        success: function (data) {
-    //            if (data != "") {
-    //                if (data.status == "OK") {
-    //                    if (Number(data.count) > 0) {
-    //                        console.log("実行できたで")
-    //                        console.log(data.html)
-    //                    } else {
-    //                        console.log("うーむ....")
-    //                    };
-    //                } else {
-    //                    alert(data.status);
-    //                };
-    //            };
-    //        }
-    //    });
-    //}
